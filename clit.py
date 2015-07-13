206fac4954d7e7f0bec3fb9ff948f5abcc440bef
Malik Butler <mb11r@my.fsu.edu>
2015-07-13 10:55:16 -0400

    added displaying user's timeline and hashtag, cleaned up formatting, DMs still not working

--- clit.py
+++ clit.py
@@ -21,51 +21,56 @@ api = twitter.Api(consumer_key='6Dy73JL5yIgC6oAfOwjQpHQYu',
 
 def parse_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-f", "--feed", type=int,
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument("-f", "--feed", type=int,
 		help="display passed argument's amount of last tweets, up to 20")
-	parser.add_argument("-t", "--tweet", help="tweet from the command line")
-	parser.add_argument("-dm", "--directmessage",
+	group.add_argument("-t", "--tweet", help="tweet from the command line")
+	group.add_argument("-d", "--directmessage",
 		help="directmessage a user from the command line")
-	parser.add_argument("-rm", "--readmessage", type=int,
-		help="display passed argument's amount of last DMs")
+#	group.add_argument("-rm", "--readmessage", type=int,
+#		help="display passed argument's amount of last DMs")
+	group.add_argument("-u", "--user",	help="display last 5 tweets from user")
+	group.add_argument("-3", "--hashtag", help="display last 5 tweets with the same hashtag")
 	opts = parser.parse_args()
 	return opts
 
-
-""" to get the home timeline for a given user (I know my own username) """
-#statuses = api.GetUserTimeline(screen_name="_malikbutler")
-#print [s.text for s in statuses]
-
-""" prints a bunch of junk """
-#print api.VerifyCredentials()
-
-""" to post a status update """
-# status = api.PostUpdate(raw_input("Status >> "))
-# print status.text
-
-def feed(amount):
-	timeline = api.GetHomeTimeline(count=amount, trim_user=True,
-		exclude_replies=True, contributor_details=False,
+def feed(amount=5):
+	if amount > 20:
+		amount = 20
+	elif amount < 1:
+		amount = 1
+	timeline = api.GetHomeTimeline(count=amount, contributor_details=False,
 		include_entities=False)
-	#timeline.split('u[').split('],')
-
 	for t in timeline:
-		print t.text
+		print "%s: %s\n" % (t.GetUser().GetScreenName(), t.GetText())
 
 def tweet(tweet):
 	tweet = api.PostUpdate(tweet)
-	print tweet.text
+	print "You just tweeted: %s" % tweet.GetText()
 
 def direct_message(user):
 	message = raw_input("Enter your message: ")
 	dm = api.PostDirectMessage(text=message, screen_name=user)
-	print "Sent: %s" % dm.text
+	print "Sent: %s" % dm.GetText()
 	print "To: @%s" % user
 
-def read_message(amount):
-	messages = api.GetDirectMessages(count=amount, include_entities=False)
-	for m in messages:
-		print m.text
+#def read_message(amount):
+#	messages = api.GetDirectMessages(count=amount, include_entities=False)
+#	for m in messages:
+#		print m.text
+
+def read_user(user):
+	timeline = api.GetUserTimeline(screen_name=user, count=5, trim_user=False)
+	for t in timeline:
+		print "%s: %s" % (t.GetUser().GetScreenName(), t.GetText())
+
+def hashtag(query):
+	query = "#" + query
+	hashtag = api.GetSearch(term=query, count=5)
+	print "Showing results for %s\n" % query
+	for h in hashtag:
+		print "%s: %s\n" % (h.GetUser().GetScreenName(), h.GetText())
+
 
 opts = parse_args()
 
@@ -75,7 +80,11 @@ elif opts.tweet:
 	tweet(opts.tweet)
 elif opts.directmessage:
 	direct_message(opts.directmessage)
-elif opts.readmessage:
-	read_message(opts.readmessage)
+#elif opts.readmessage:
+#	read_message(opts.readmessage)
+elif opts.user:
+	read_user(opts.user)
+elif opts.hashtag:
+	hashtag(opts.hashtag)
 else:
 	print "pointless"
