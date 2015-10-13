ac1847105ca2c59284ec1efe4968f8476eff634c
roninb <mb11r@my.fsu.edu>
2015-10-13 01:24:52 -0400

    made clit.py generic and tested functionality.

--- clit.py
+++ clit.py
@@ -12,11 +12,13 @@
 import argparse
 import twitter
 
-api = twitter.Api(consumer_key='6Dy73JL5yIgC6oAfOwjQpHQYu',
-	consumer_secret='7sbxr51R36ZZtpX9Idx7NwCNdgtzFlidk7fvDEWYwLjqRqZMhv',
-	access_token_key='189396089-QhunfcJUhrMAyVxv2I7J9a2MmBERIrxDP3oZ0OH9',
-	access_token_secret='kp6kmJ7fQiF7ZLkJH0qWgIuOsADdBu6g3HoOxUQSh6L8A')
+# establishes connection to twitter api
+api = twitter.Api(consumer_key='<ROLL>',
+	consumer_secret='<YOUR>',
+	access_token_key='<OWN>',
+	access_token_secret='<KEYS>')
 
+# parses commandline arguments and delivers help messages
 def parse_args():
 	parser = argparse.ArgumentParser()
 	group = parser.add_mutually_exclusive_group()
@@ -25,13 +27,12 @@ def parse_args():
 	group.add_argument("-t", "--tweet", help="tweet from the command line")
 	group.add_argument("-d", "--directmessage",
 		help="directmessage a user from the command line")
-#	group.add_argument("-rm", "--readmessage", type=int,
-#		help="display passed argument's amount of last DMs")
 	group.add_argument("-u", "--user",	help="display last 5 tweets from user")
 	group.add_argument("-3", "--hashtag", help="display last 5 tweets with the same hashtag")
 	opts = parser.parse_args()
 	return opts
 
+# grabs a certain amount of tweets from feed
 def feed(amount=5):
 	if amount > 20:
 		amount = 20
@@ -40,34 +41,33 @@ def feed(amount=5):
 	timeline = api.GetHomeTimeline(count=amount, contributor_details=False,
 		include_entities=False)
 	for t in timeline:
-		print "%s: %s\n" % (t.GetUser().GetScreenName(), t.GetText())
+		print "%s: %s\n" % (t.GetUser().GetScreenName().encode("utf-8"), t.GetText().encode("utf-8"))
 
+# posts a tweet
 def tweet(tweet):
 	tweet = api.PostUpdate(tweet)
-	print "You just tweeted: %s" % tweet.GetText()
+	print "You just tweeted: %s" % tweet.GetText().encode("utf-8")
 
+# direct messages a user
 def direct_message(user):
 	message = raw_input("Enter your message: ")
 	dm = api.PostDirectMessage(text=message, screen_name=user)
-	print "Sent: %s" % dm.GetText()
+	print "Sent: %s" % dm.GetText().encode("utf-8")
 	print "To: @%s" % user
 
-#def read_message(amount):
-#	messages = api.GetDirectMessages(count=amount, include_entities=False)
-#	for m in messages:
-#		print m.text
-
+# grabs a user's last 5 tweets
 def read_user(user):
 	timeline = api.GetUserTimeline(screen_name=user, count=5, trim_user=False)
 	for t in timeline:
-		print "%s: %s" % (t.GetUser().GetScreenName(), t.GetText())
+		print "%s: %s" % (t.GetUser().GetScreenName().encode("utf-8"), t.GetText().encode("utf-8"))
 
+# grabs a hashtags last 5 usages
 def hashtag(query):
 	query = "#" + query
 	hashtag = api.GetSearch(term=query, count=5)
 	print "Showing results for %s\n" % query
 	for h in hashtag:
-		print "%s: %s\n" % (h.GetUser().GetScreenName(), h.GetText())
+		print "%s: %s\n" % (h.GetUser().GetScreenName().encode("utf-8"), h.GetText().encode("utf-8"))
 
 
 opts = parse_args()
@@ -78,11 +78,9 @@ elif opts.tweet:
 	tweet(opts.tweet)
 elif opts.directmessage:
 	direct_message(opts.directmessage)
-#elif opts.readmessage:
-#	read_message(opts.readmessage)
 elif opts.user:
 	read_user(opts.user)
 elif opts.hashtag:
 	hashtag(opts.hashtag)
 else:
-	print "pointless"
+	print "You didn't enter an argument!!"
